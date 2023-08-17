9741e57677b91ca393d266f96fcca0be17ff019d
ulaali <ulaali513@yahoo.com>
2023-08-17 13:36:26 +0300

    meme-generator app

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,52 +1,127 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import Context from "../Stor";
 import "../App.css";
+import { HuePicker } from "react-color";
+
 function Home() {
+  const [color, setColor] = useState("#fff");
+  const [italic, setItalic] = useState(false);
   const infos = useContext(Context);
+  console.log(infos.selected_img);
+
   return (
     <div>
       <h1>Meme generator App</h1>
-      <input
-        type="text"
-        value={infos.caption}
-        onChange={(e) => infos.setCaption(e.target.value)}
-        placeholder="caption"
-      ></input>
-      {/* <input
-        type="text"
-        value={infos.caption2}
-        onChange={(e) => infos.setCaption2(e.target.value)}
-        placeholder="caption two"
-      ></input> */}
+      <div className="inputs">
+        {" "}
+        <input
+          type="text"
+          value={infos.caption}
+          onChange={(e) => infos.setCaption(e.target.value)}
+          placeholder="caption"
+        ></input>
+        <button
+          onClick={() => {
+            setItalic(!italic);
+          }}
+        >
+          Change fontStyle
+        </button>
+      </div>
+      <br></br>
+
+      <div className="color-picker">
+        <HuePicker
+          color={color}
+          onChangeComplete={(color) => setColor(color.hex)}
+        />
+      </div>
       <br></br>
       <div className="selected_meme">
-        <img src={infos.simg.ssimg} ></img>
+        <img src={infos.selected_img.ssimg}></img>
       </div>
-      <p style={infos.simg.index===0? {position:'absolute',top:"18%",right:"46%",color:"white"}:{display:"none"}}>{infos.caption}</p>
-      <p style={infos.simg.index===1? {position:'absolute',top:"25%",right:"46%",color:"white"}:{display:"none"}}>{infos.caption}</p>
-      <p style={infos.simg.index===2? {position:'absolute',top:"60%",right:"44%",color:"white"}:{display:"none"}}>{infos.caption}</p>
-      <p style={infos.simg.index===3? {position:'absolute',top:"50%",right:"42%",color:"black"}:{display:"none"}}>{infos.caption}</p>
+      <p
+        style={
+          infos.selected_img.index === 0
+            ? {
+                position: "absolute",
+                top: "30%",
+                right: "46%",
+                color,
+                fontStyle: `${italic ? "italic" : "normal"}`,
+              }
+            : { display: "none" }
+        }
+      >
+        {infos.caption}
+      </p>
+      <p
+        style={
+          infos.selected_img.index === 1
+            ? {
+                position: "absolute",
+                top: "38%",
+                right: "45%",
+                color,
+                fontStyle: `${italic ? "italic" : "normal"}`,
+              }
+            : { display: "none" }
+        }
+      >
+        {infos.caption}
+      </p>
+      <p
+        style={
+          infos.selected_img.index === 2
+            ? {
+                position: "absolute",
+                top: "72%",
+                right: "44%",
+                color,
+                fontStyle: `${italic ? "italic" : "normal"}`,
+              }
+            : { display: "none" }
+        }
+      >
+        {infos.caption}
+      </p>
+      <p
+        style={
+          infos.selected_img.index === 3
+            ? {
+                position: "absolute",
+                top: "62%",
+                right: "42%",
+                color,
+                fontStyle: `${italic ? "italic" : "normal"}`,
+              }
+            : { display: "none" }
+        }
+      >
+        {infos.caption}
+      </p>
 
       <div className="memes">
-      <div><h3>Select a meme to add capton ^*^</h3></div>  
-       <div className="meme">
-       {infos.imgs.map((img,i) => {
-          return (
-            <img
-              src={img}
-              key={i}
-              onClick={() =>{const newimg={...infos.simg}
-              newimg.ssimg=img
-              newimg.index=i
-              infos.setImg(newimg)
-              infos.setCaption('')
-            }
-            }
-              style={{ width: "200px", height: "200px" }}
-            />
-          );
-        })}
-        </div> 
+        <div>
+          <h3>Select a meme to add capton ^*^</h3>
+        </div>
+        <div className="meme">
+          {infos.imgs.map((img, i) => {
+            return (
+              <img
+                src={img}
+                key={i}
+                onClick={() => {
+                  const newimg = { ...infos.selected_img };
+                  newimg.ssimg = img;
+                  newimg.index = i;
+                  infos.setImg(newimg);
+                }}
+                style={{ width: "200px", height: "200px" }}
+              />
+            );
+          })}
+        </div>
       </div>
     </div>
   );
--- src/Stor.jsx
+++ src/Stor.jsx
@@ -8,9 +8,9 @@ export function DataProvider({children}){
        imgs.push(require(`./assets/meme${index}.png`));
     }
     const [caption,setCaption]=useState("");
-    const [caption2,setCaption2]=useState("");
-    const [simg,setImg]=useState([{ssimg:<img src={imgs[1]}/>,index:1}]);
-    return <Context.Provider value={{caption,setCaption,caption2,setCaption2,imgs,simg,setImg}}>{children}</Context.Provider>
+    const [selected_img,setImg]=useState({ssimg:imgs[1],index:1});
+
+    return <Context.Provider value={{caption,setCaption,imgs,selected_img,setImg}}>{children}</Context.Provider>
 }
 
 export default  Context;
