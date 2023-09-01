1c115afc8619c9d14644bedcbc9c43c9c952d27d
sanguine-dreams <sara0dhahir@gmail.com>
2023-09-01 18:23:38 +0300

    functionality update

--- src/components/preview.jsx
+++ src/components/preview.jsx
@@ -2,14 +2,20 @@ import { useState } from "react";
 import { useContext } from "react";
 import { MemeContext } from "../store/store";
 
-function Preview(){
-const {styling, setStyling} = useContext(MemeContext);
- return (<div className="preview">
-  <img src={styling.imgUrl}/>
-  <h1
-  style={{color: styling.color, fontSize: styling.size}}>
-    {styling.topText}</h1>
- </div>)
+function Preview() {
+  const { styling, setStyling } = useContext(MemeContext);
+
+  return (
+    <div className="preview">
+      <img id="img-preview" src={styling.imgUrl} />
+      <h1
+        id="meme-text"
+        style={{ color: styling.color, fontSize: styling.size }}
+      >
+        {styling.topText}
+      </h1>
+    </div>
+  );
 }
 
 export default Preview;
